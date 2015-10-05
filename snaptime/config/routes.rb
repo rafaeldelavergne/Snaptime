f350d5bbfbc919c0d0d25c93312d8c989154cd71
rafael de Lavergne <rafdldl@hotmail.fr>
2015-10-05 20:37:20 +0200

    un message de commit

--- snaptime/config/routes.rb
+++ snaptime/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :shows
   get 'home/index'
   root 'home#index'
+  post 'shows/:id/book' => 'shows#book'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
